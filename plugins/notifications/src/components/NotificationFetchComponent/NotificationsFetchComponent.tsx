7df8a28eb4268ad584a4988d069c4d5551c8a0a4
Athira-Sabu <athirasudhaks@gmail.com>
2024-06-07 15:18:20 +0300

    refactor: Seperated UI components

--- plugins/notifications/src/components/NotificationFetchComponent/NotificationsFetchComponent.tsx
+++ plugins/notifications/src/components/NotificationFetchComponent/NotificationsFetchComponent.tsx
@@ -1,151 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {useApi} from '@backstage/core-plugin-api';
 import {
-    Table,
-    TableColumn,
     Progress,
-    ResponseErrorPanel, TableFilter,
+    ResponseErrorPanel
 } from '@backstage/core-components';
 import useAsync from 'react-use/lib/useAsync';
 import {notificationsApiRef} from "../../api/NotificationsApi";
 import {useSignal} from '@backstage/plugin-signals-react';
 import {Notification} from "../../types";
-import {Box, Button, Checkbox, IconButton, Link } from '@material-ui/core';
-import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
-import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
-import DoneIcon from '@mui/icons-material/Done';
-import DoneAllIcon from '@mui/icons-material/DoneAll';
-import ChecklistRtlIcon from '@mui/icons-material/ChecklistRtl';
-import Tooltip from '@mui/material/Tooltip';
-
-type DenseTableProps = {
-    notifications: Notification[];
-    onLoadMore: () => void;
-    onDelete: (ids: number[]) => void;
-    updateStatus: (ids: number[], status: boolean) => void;
-};
-
-
-export const DenseTable = ({notifications, onLoadMore, onDelete, updateStatus}: DenseTableProps) => {
-    const [showMore, setShowMore] = useState<Record<string, boolean>>({});
-    const [selected, setSelected] = useState<number[]>([]);
-
-    const handleSelect = (id: number) => {
-        if (selected.includes(id)) {
-            setSelected(selected.filter(item => item !== id));
-        } else {
-            setSelected([...selected, id]);
-        }
-    };
-    const columns: TableColumn[] = [
-        {
-            title: '',
-            field: 'delete',
-            render: (rowData: any) => (
-                <Checkbox checked={selected.includes(rowData.id)} onChange={() => handleSelect(rowData.id)}/>
-            )
-        },
-        {
-            title: 'Title',
-            field: 'title',
-            render: (rowData: any) => (
-                <>
-                    <strong>{rowData.title}</strong>
-                    <p>
-                        {showMore[rowData.id] ? rowData.message : rowData.message.substring(0, 100)}
-                        {rowData.message.length > 100 && (
-                            <div
-                                onClick={() => setShowMore(prev => ({...prev, [rowData.id]: !prev[rowData.id]}))}
-                                style={{color: 'blue', textDecoration: 'underline', cursor: 'pointer', marginTop: 5}}
-                            >
-                                {showMore[rowData.id] ? 'Show less' : 'Show more'}
-                            </div>
-                        )}
-                    </p>
-                </>
-            )
-        },
-        {title: 'Origin', field: 'origin'},
-        {title: 'Priority', field: 'priority'},
-        {
-            title: 'Actions',
-            field: 'actions',
-            render: (rowData: any) => (
-                <>
-                    <IconButton onClick={() => updateStatus([rowData.id], !rowData.read)}>
-                        {rowData.read ? <DoneAllIcon sx={{ color: '#0784c7' }}/> : <DoneIcon/>}
-                    </IconButton>
-                    <IconButton onClick={() => onDelete([rowData.id])}>
-                        <DeleteOutlinedIcon/>
-                    </IconButton>
-                </>
-            )
-        },
-    ];
-
-    const data = notifications ? notifications.map(notification => {
-        return {
-            id: notification.id,
-            title: notification.title,
-            message: notification.message,
-            origin: notification.origin,
-            read: Boolean(notification.read),
-            priority: notification.priority,
-        };
-    }) : [];
-
-    const filters: TableFilter[] = [
-        {
-            column: 'Origin',
-            type: 'multiple-select',
-        },
-        {
-            column: 'Priority',
-            type: 'select',
-        }
-    ];
-
-    return (
-        <>
-            <Table
-                key={notifications?.length}
-                title={
-                    <Box display="flex" alignItems="center">
-                        {selected.length > 0 && (
-                            <>
-                            <Tooltip title="Mark all selcted as read">
-                                <IconButton onClick={() => updateStatus(selected, true)}>
-                                    <ChecklistRtlIcon />
-                                </IconButton>
-                            </Tooltip>
-                            <Tooltip title="Delete all selected">
-                            <IconButton onClick={() => onDelete(selected)}>
-                                <DeleteSweepIcon />
-                            </IconButton>
-                            </Tooltip>
-                            </>
-                        )}
-                        <Link
-                            component="button"
-                            onClick={onLoadMore}
-                            style={{minWidth: '100px'}}
-                        >
-                            Load more...
-                        </Link>
-                    </Box>
-                }
-                options={{search: false, padding: 'dense', paging: false}}
-                columns={columns}
-                data={data}
-                filters={filters}
-            />
-            <Box display="flex" justifyContent="flex-end" marginTop={2}>
-                <Button onClick={onLoadMore}>Load More</Button>
-            </Box>
-
-        </>
-    );
-};
+import { NotificationsTable } from './NotificationsTable';
 
 export const NotificationsFetchComponent = () => {
     const notificationApi = useApi(notificationsApiRef);
@@ -189,7 +52,7 @@ export const NotificationsFetchComponent = () => {
         return <ResponseErrorPanel error={error}/>;
     }
     return (
-        <DenseTable notifications={notifications || []} onLoadMore={ async() => await fetchNotifications(cursor)}
+        <NotificationsTable notifications={notifications || []} loadMore={ async() => await fetchNotifications(cursor)}
         onDelete={deleteNotifications} updateStatus={updateStatus}/>
     );
 };
---
+++ plugins/notifications/src/components/NotificationFetchComponent/NotificationsTable.tsx
@@ -0,0 +1,173 @@
+import {Box, Checkbox, IconButton, Link} from '@material-ui/core';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
+import DoneIcon from '@mui/icons-material/Done';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import ChecklistRtlIcon from '@mui/icons-material/ChecklistRtl';
+import Tooltip from '@mui/material/Tooltip';
+import {useCallback, useState} from 'react';
+import {Notification} from "../../types";
+import {
+    Table,
+    TableColumn,
+    TableFilter,
+} from '@backstage/core-components';
+import React from 'react';
+
+interface NotificationsTableProps {
+    notifications: Notification[];
+    loadMore: () => void;
+    onDelete: (ids: number[]) => void;
+    updateStatus: (ids: number[], status: boolean) => void;
+};
+
+interface TitleProps {
+    title: string;
+    message: string;
+}
+
+interface SelectedActionsProps {
+    selected: number[];
+    updateStatus: (ids: number[], status: boolean) => void;
+    onDelete: (ids: number[]) => void;
+}
+
+interface LoadMoreProps {
+    loadMore: () => void;
+}
+
+const Title = ({title, message}: TitleProps) => {
+    const [showMore, setShowMore] = useState(false);
+
+    return (
+        <>
+            <strong>{title}</strong>
+            <p>
+                {showMore ? message : message.substring(0, 100)}
+                {message.length > 100 && (
+                    <div
+                        onClick={() => setShowMore(!showMore)}
+                        style={{color: '#0784c7', textDecoration: 'underline', cursor: 'pointer', marginTop: 5}}
+                    >
+                        {showMore ? 'Show less' : 'Show more'}
+                    </div>
+                )}
+            </p>
+        </>
+    );
+};
+
+const SelectedActions = ({selected, updateStatus, onDelete}: SelectedActionsProps) => (
+    <Box display="flex" alignItems="center">
+        {selected.length > 0 && (
+            <>
+                <Tooltip title="Mark all selected as read">
+                    <IconButton onClick={() => updateStatus(selected, true)}>
+                        <ChecklistRtlIcon/>
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Delete all selected">
+                    <IconButton onClick={() => onDelete(selected)}>
+                        <DeleteSweepIcon/>
+                    </IconButton>
+                </Tooltip>
+            </>
+        )}
+    </Box>
+);
+
+const LoadMore = ({loadMore}: LoadMoreProps) => (
+    <Box display="flex" justifyContent="flex-end" marginTop={2}>
+        <Link
+            component="button"
+            onClick={loadMore}
+            style={{minWidth: '100px'}}
+        >
+            Load more...
+        </Link>
+    </Box>
+);
+
+export const NotificationsTable = ({notifications, loadMore, onDelete, updateStatus}: NotificationsTableProps) => {
+    const [selected, setSelected] = useState<number[]>([]);
+
+    const handleSelect = useCallback((id: number) => {
+        setSelected(prevSelected => prevSelected.includes(id)
+            ? prevSelected.filter(item => item !== id) : [...prevSelected, id]);
+    }, []);
+
+    const renderActions = (rowData: any) => (
+        <>
+            <IconButton onClick={() => updateStatus([rowData.id], !rowData.read)}>
+                {rowData.read ? <DoneAllIcon sx={{color: '#0784c7'}}/> : <DoneIcon/>}
+            </IconButton>
+            <IconButton onClick={() => onDelete([rowData.id])}>
+                <DeleteOutlinedIcon/>
+            </IconButton>
+        </>
+    );
+
+    const columns: TableColumn[] = [
+        {
+            title: '',
+            field: 'checkbox',
+            render: (rowData: any) => (
+                <Checkbox checked={selected.includes(rowData.id)} onChange={() => handleSelect(rowData.id)}/>
+            )
+        },
+        {
+            title: 'Title',
+            field: 'title',
+            render: (rowData: any) => <Title title={rowData.title} message={rowData.message}/>
+        },
+        {
+            title: 'Origin',
+            field: 'origin'
+        },
+        {
+            title: 'Priority',
+            field: 'priority'
+        },
+        {
+            title: 'Actions',
+            field: 'actions',
+            render: renderActions,
+        },
+    ];
+
+    const data = notifications ? notifications.map(notification => {
+        return {
+            id: notification.id,
+            title: notification.title,
+            message: notification.message,
+            origin: notification.origin,
+            read: Boolean(notification.read),
+            priority: notification.priority,
+        };
+    }) : [];
+
+    const filters: TableFilter[] = [
+        {
+            column: 'Origin',
+            type: 'multiple-select',
+        },
+        {
+            column: 'Priority',
+            type: 'select',
+        }
+    ];
+
+    return (
+        <>
+            <Table
+                key={notifications?.length}
+                title={<SelectedActions selected={selected} updateStatus={updateStatus} onDelete={onDelete}/>}
+                options={{search: false, padding: 'dense', paging: false}}
+                columns={columns}
+                data={data}
+                filters={filters}
+            />
+            <LoadMore loadMore={loadMore}/>
+        </>
+    );
+};
